3f89d681d026911253b56118203c16ae89059575
arshia188 <arshia188@gmail.com>
2017-08-14 00:41:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
+//change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+//move the image
+
+var img=document.getElementById('img');
+img.onclick=function() {
+    img.style.marginLeft='100px';
+};
