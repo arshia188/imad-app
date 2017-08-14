05a44f5ea964c46641c535fe437d092f96c04afa
arshia188 <arshia188@gmail.com>
2017-08-14 21:01:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,22 +13,17 @@ request.onreadystatechange=function(){
   if(request.readyState==XMLHttpRequest.DONE){
       //action
       if(request.status==200){
-          var names=request.responseText;
-          names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+          
       }
   }  
 };
 
 //make request
 
-request.open('GET','http://arshia188.imad.hasura-app.io/submit-name?name='+name,true);
+request.open('GET','http://arshia188.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
@@ -39,8 +34,29 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     //make a request to the server and send the name
+    var request=new XMLHttpRequest();
+
+
+//capture the response and store it in a variable
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      //action
+      if(request.status==200){
+          var names=request.responseText;
+          names=JSON.parse(names);
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
     
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+      }
+  }
     //capture the list of names and render it as a list
     
+};
+request.open('GET','http://arshia188.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
 };
 
