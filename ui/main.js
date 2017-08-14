a3f7c29a3b7bc80acc40b14d02b3a38ed92b924f
arshia188 <arshia188@gmail.com>
2017-08-14 19:58:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,25 @@ var button=document.getElementById('counter');
 var counter=0;
 
 button.onclick=function(){
-//make a request  to the counter endpoint
+//create a request object
+
+var request=new XMLHttpRequest();
+
 
 //capture the response and store it in a variable
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      //action
+      if(request.status==200){
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+      }
+  }  
+};
 
-//Render the variable in the correct span;
+//make request
 
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+request.open('GET','http://arshia188.imad.hasura-app.io/counter',true);
+request.send(null);
 };
