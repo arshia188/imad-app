9e8d9823591046e0ae99dfeb1b5b81f63d5308be
arshia188 <arshia188@gmail.com>
2017-08-14 20:46:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,16 +13,22 @@ request.onreadystatechange=function(){
   if(request.readyState==XMLHttpRequest.DONE){
       //action
       if(request.status==200){
-          var counter=request.responseText;
-          var span=document.getElementById('count');
-          span.innerHTML=counter.toString();
+          var names=request.responseText;
+          names=JSON.parse(names);
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
       }
   }  
 };
 
 //make request
 
-request.open('GET','http://arshia188.imad.hasura-app.io/counter',true);
+request.open('GET','http://arshia188.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 };
 
@@ -35,13 +41,6 @@ submit.onclick=function(){
     //make a request to the server and send the name
     
     //capture the list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
     
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
 };
 
