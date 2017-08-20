75f67fa5db398719b429c826a3d811e032f3873f
arshia188 <arshia188@gmail.com>
2017-08-20 11:23:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('path');
 var app = express();
 app.use(morgan('combined'));
-
+var config={
+    user: 'arshia188',
+    database:'arshia188',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env. DB_PASSWORD
+    
+};
 var articles={
     
 'article-one':{
@@ -69,6 +76,19 @@ var htmlTemplate=`
 
 return htmlTemplate;
 }
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res)){
+    //make a select request
+    //return the response with the result.
+    pool.query('SELECT * FROM test',function(err,result)){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 var counter=0;
 app.get('/counter',function(req, res){
